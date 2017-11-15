9a0d5e49e10bfa27690c5ab8f0b6513df40bfaed
Loren Kuich <loren.jk3@gmail.com>
2017-11-15 05:49:07 -0800

    Add project files.

---
+++ ControlHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ControlHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
