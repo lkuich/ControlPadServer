eadb95a7b376dce56715940368dea5477dfc1a49
Loren Kuich <loren.jk3@gmail.com>
2017-11-29 23:34:14 -0800

    Worked on cleanup

--- BroadcastServer/BroadcastServer.cs
+++ BroadcastServer/BroadcastServer.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
-using Grpc.Core;
-using Service;
 
 public static class IPAddressExtensions
 {
