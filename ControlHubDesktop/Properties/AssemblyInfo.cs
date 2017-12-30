e156d91d53aad63e309c7039ff1055fce7b85e41
Loren Kuich <loren.jk3@gmail.com>
2017-12-30 14:31:15 -0700

    Renamed

--- ControlHubDesktop/Properties/AssemblyInfo.cs
+++ ControlHubDesktop/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ControlHubDesktop")]
+[assembly: AssemblyTitle("ControlPad Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ControlHubDesktop")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("ControlPad Server")]
+[assembly: AssemblyCopyright("Copyright ©lkuich 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
